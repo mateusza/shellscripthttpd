86c1b4debc2e1e2ebfd8bceed5d1c4f595be8edc
Mateusz Adamowski <mateusz@adamowski.pl>
2014-03-05 23:22:38 +0100

    the source code

---
+++ httpd.sh
@@ -0,0 +1,292 @@
+#!/bin/sh
+
+# copyright (C) 2014 Mateusz Adamowski <mateusz at adamowski dot pl>
+#
+# This code is public domain. You are free to use it anywhere you want.
+# Please drop a line once you find it useful.
+#
+# Features:
+#  * works with inetd
+#  * supports HTTP/1.0
+#  * support GET, POST and HEAD methods
+#  * follows POST/Redirect/GET pattern
+#  * single file
+#  * exports request header according to CGI standard: HTTP_
+#
+# initial settings
+
+SERVER_VERSION="shellscripthttpd/0.1.0"
+SERVER_PROTOCOL="HTTP/1.0"
+CHARSET="UTF-8"
+CONTENT_TYPE="text/html; charset=$CHARSET"
+
+RESPONSE_HEADERS_FILE="/tmp/http-response-headers.$$.txt"
+RESPONSE_FILE="/tmp/http-response.$$.txt"
+REQUEST_HEADERS_FILE="/tmp/http-request-headers.$$.txt"
+REQUEST_BODY="/tmp/http-request.$$.txt"
+ROUTES_FILE="/tmp/http-routes.$$.txt"
+
+# functions
+
+response_name(){
+    [ "$1" = "200" ] && echo "OK"
+    [ "$1" = "404" ] && echo "Not found"
+    [ "$1" = "500" ] && echo "Internal Server Error"
+    [ "$1" = "303" ] && echo "See other"
+}
+
+add_header(){
+    echo "$1: $2" >> $RESPONSE_HEADERS_FILE
+}
+
+add_route(){
+    echo "$1 $2" >> $ROUTES_FILE
+}
+
+require_POST(){
+    if [ "$REQUEST_METHOD" != "POST" ]
+    then
+        CODE="500"
+        VIEW="ERROR500"
+        response
+        exit
+    fi    
+}
+
+read_post_var(){
+    local var
+    var="$1"
+    grep -o "\&$var=[^\&]\+" < $REQUEST_BODY | sed -e 's/.*=//' | urldecode
+}
+
+cleanup(){
+    rm -f "$RESPONSE_HEADERS_FILE"
+    rm -f "$REQUEST_HEADERS_FILE"
+    rm -f "$ROUTES_FILE"
+    rm -f "$RESPONSE_FILE"
+    #rm -f "$REQUEST_BODY"
+}
+
+request(){
+    local hname
+    local hvalue
+
+    touch "$RESPONSE_HEADERS_FILE"
+    touch "$REQUEST_HEADERS_FILE"
+    touch "$ROUTES_FILE"
+    touch "$RESPONSE_FILE"
+    touch "$REQUEST_BODY"
+
+    read REQUEST_METHOD REQUEST_URI CLIENT_PROTOCOL
+    SCRIPT_NAME="$( echo "$REQUEST_URI" | grep -o '^[^\?]\+' )"
+
+    if echo "$REQUEST_URI" | grep '\?' > /dev/null
+    then
+        QUERY_STRING="$( echo "$REQUEST_URI" | sed -e 's/^.*?//' )"
+    else
+        QUERY_STRING=""
+    fi
+
+    while read -r line
+    do
+        line=$( echo $line | tr -d '\r' )
+        [ -z "$line" ] && break
+        echo "$line" >> $REQUEST_HEADERS_FILE
+        hname="$( echo $line | grep -o '^[-A-Za-z]\+' | tr a-z A-Z | sed -e 's/-/_/g' )"
+        hvalue="$( echo $line | sed -e 's/^.*: //' )"
+        eval "HTTP_$hname='$hvalue'"
+    done
+
+    if [ "x$HTTP_CONTENT_LENGTH" != "x" ]
+    then
+        echo -n '&' > $REQUEST_BODY
+        head -c $HTTP_CONTENT_LENGTH >> $REQUEST_BODY
+        echo -n '&' >> $REQUEST_BODY
+    fi
+
+# TODO read data if Content-Length specified
+#    
+#    echo "Len: $HTTP_CONTENT_LENGTH"
+#    cat > /tmp/dupa.txt
+}
+
+response(){
+    view_$VIEW > $RESPONSE_FILE
+    add_header "Content-Length" "$( awk 'sub("$", "\r")' < $RESPONSE_FILE | wc -c )"
+
+    echo "$SERVER_PROTOCOL $CODE $( response_name $CODE )"
+    cat "$RESPONSE_HEADERS_FILE"
+    echo ""
+    [ "$REQUEST_METHOD" = "HEAD" ] && return
+    cat "$RESPONSE_FILE"
+}
+
+route(){
+    while read route action
+    do
+        if echo "$SCRIPT_NAME" | grep -o "$route" > /dev/null
+        then
+            CODE=200
+            ACTION="$action"
+            break
+        fi
+    done < $ROUTES_FILE
+
+    VIEW=$ACTION
+    if [ "x$CODE" = "x" ]
+    then
+        CODE="404"
+        ACTION=ERROR404
+        VIEW=ERROR404
+    fi
+    action_$ACTION
+}
+
+urldecode(){
+      /usr/bin/printf "$(sed 's/+/ /g;s/%\(..\)/\\x\1/g;')"
+}
+
+action(){
+    DATE=$(date +"%a, %d %b %Y %H:%M:%S %Z")
+    add_header "Date" "$DATE"
+    add_header "Connection" "close"
+    add_header "Server" "$SERVER_VERSION"
+    add_header "Content-Type" "$CONTENT_TYPE"
+}
+
+_e(){
+    sed -e '
+    s_&_\&amp;_g;
+    s_<_\&lt;_g;
+    s_>_\&gt;_g;
+    s_"_\&quot;_g;
+    '"s_'_\&apos;_g"
+}
+
+run(){
+    request
+    action
+    route
+    response | awk 'sub("$", "\r")'
+    cleanup
+}
+
+redirect(){
+    REDIRECTION_LOCATION="$1"
+    add_header "Location" "$REDIRECTION_LOCATION"
+    VIEW="REDIRECT"
+    CODE=303
+}
+
+view_ERROR500(){
+    echo "<h1>ERROR 500</h1>"
+}
+
+view_ERROR404(){
+    echo "<h1>404 Not found</h1>"
+}
+
+action_ERROR404(){
+    echo -n
+}
+
+view_REDIRECT(){
+    echo "<h1>redirecting... <code>$REDIRECTION_LOCATION</code></h1>"
+}
+
+#
+# CUSTOM ACTIONS AND VIEWS
+#
+
+action_test1(){
+    XXX="`date`"
+}
+
+action_source(){
+    echo -n
+}
+action_say(){
+    require_POST
+    redirect '/'
+    echo action | espeak
+}
+
+view_test1(){
+    echo "<html>"
+    echo "<h1>$XXX</h1>"
+    echo "<pre>HTTP_HOST"
+    echo "$( echo $HTTP_HOST | _e )</pre>"
+    echo "<pre>HTTP_USER_AGENT"
+    echo "$( echo $HTTP_USER_AGENT | _e )</pre>"
+    echo "<table border=1>"
+    while read -r h
+    do
+      echo "<tr>"
+      echo "<td><code>"
+      echo "$h" | grep -o '^[-A-Za-z]\+' | _e
+      echo "</code></td>"
+      echo "<td><code>"
+      echo "$h" | sed -e 's/^.*: //' | _e 
+      echo "</code></td></tr>"
+    done < $REQUEST_HEADERS_FILE
+    echo "</table>"
+    echo "<h2><code>[$REQUEST_METHOD] [$SCRIPT_NAME] [$SERVER_PROTOCOL]</code></h2>"
+    echo "<pre>params: $QUERY_STRING</pre>"
+    echo "<a href='/source/'>Source</a>"
+    echo "<form action='/x/say' method=POST>"
+    echo "<input type=submit>"
+    echo "</form>"
+    echo "</html>"
+}
+
+view_source(){
+    echo "<html>"
+    echo "<textarea rows=10 cols=60>"
+    cat httpd.sh | _e 
+    echo "</textarea>"
+    echo "</html>"
+}
+
+action_form1(){
+    echo -n
+}
+
+view_form1(){
+    echo '<pre>'
+    touch /tmp/chat.txt
+    tail -20 /tmp/chat.txt | _e
+    echo '</pre>'
+    echo '<form method=POST action="save">'
+    echo '<input name=a>'
+    echo '<input name=bb>'
+    echo '<input type=submit>'
+    echo '</form>'
+}
+
+action_form1_save(){
+    require_POST
+    redirect '/form1/'
+    a=$( read_post_var a )
+    bb=$( read_post_var bb )
+    echo "$a: $bb" >> /tmp/chat.txt
+}
+
+view_form1_save(){
+    echo DUPA
+}
+##
+## ROUTES
+##
+
+add_route '^/$'             'test1'
+add_route '^/source/$'      'source'
+add_route '^/x/say$'        'say'
+add_route '^/form1/$'       'form1'
+add_route '^/form1/save$'   'form1_save'
+
+##
+## process the request
+##
+
+run
+
